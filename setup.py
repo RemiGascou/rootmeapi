4647606f42f0eb1f4c06a0dbb613395ecfff53b7
Rémi Gascou <remi@gascou.net>
2020-11-28 20:01:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+import setuptools
 
-setup(
-    name         = 'rootmeapi',
-    version      = '1.0',
-    description  = 'Root Me API',
-    url          = 'http://github.com/Remigascou/rootmeapi',
-    author       = 'Podalirius',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'rootmeapi',
+    version = '1.1',
+    description = 'Root Me API',
+    url = 'http://github.com/Remigascou/rootmeapi',
+    author = 'Podalirius',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
     author_email = 'podalirius@protonmail.com',
-    license      = 'GPL2',
-    packages     = [
-        'rootmeapi',
-        'rootmeapi/types'
+    packages = setuptools.find_packages(),
+    license = 'GPL2',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
     ],
-    zip_safe     = False
+    python_requires = '>=3.4',
 )
